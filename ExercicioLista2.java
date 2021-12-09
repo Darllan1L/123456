fba62e436c6789c094a188b0d67e9d17de179153
Darllan <darllan13@hotmail.com>
2021-12-09 17:43:19 -0300

    EX3

---
+++ ExercicioLista2.java
@@ -0,0 +1,41 @@
+package br.com.generation.condicionais;
+
+import java.util.Scanner;
+
+public class ExercicioLista2 {
+
+	//Faça um programa que receba três inteiros e diga qual deles é o maior.
+	public static void main(String[] args) {
+		Scanner leia = new Scanner (System.in);
+		
+		int n1, n2, n3;
+		
+		System.out.println("Digite um numero:");
+		n1 = leia.nextInt();
+		
+		System.out.println("Digite um numero:");
+		n2 = leia.nextInt();
+		
+		System.out.println("Digite um numero:");
+		n3 = leia.nextInt();
+		
+		if ( n1>n2 && n1>n3) {
+		
+		System.out.println("O maior numero é: " + n1);
+		
+		}
+		else if ( n2>n3 && n2>n1 ){
+			System.out.println("O maior numero é: " + n2);
+			
+		}
+		
+		else if ( n3>n2 && n3>n1 ) {
+			System.out.println("O maior numero é: " + n3);
+		}
+		
+		
+		
+		leia.close();
+	}
+
+}
